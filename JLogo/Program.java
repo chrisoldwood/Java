d3e3114811650220bb791b621058b82bc83bb892
Administrator <administrator@chrisoldwood.com>
2001-04-21 15:44:33 +0000

    Reworked command storage and manipulation to use an array rather than a list based approach. Added support for execution contexts. Reworked source code fetching feature.

--- JLogo/Program.java
+++ JLogo/Program.java
@@ -15,8 +15,10 @@ public class Program
 
 	public void clear()
 	{
-		m_oFirstCmd = null;
-		m_oLastCmd  = null;
+		m_vCmds.removeAll();
+		m_vStack.removeAllElements();
+
+		notifyListeners(CLEARED);
 	}
 
 	/********************************************************************************
@@ -25,65 +27,73 @@ public class Program
 
 	public void add(Cmd oCmd)
 	{
-		// Push the command onto the stack.
-		if (oCmd instanceof RepeatCmd)
+		// Not inside a repeat cmd?
+		if (m_vStack.size() == 0)
 		{
-			m_vStack.push(oCmd);
-			return;
-		}
+			m_vCmds.add(oCmd);
 
-		// Loop being created?
-		if (m_vStack.size() > 0)
+			// Start a new cmd block.
+			if (oCmd instanceof RepeatCmd)
+				m_vStack.push(oCmd);
+		}
+		// Inside a repeat cmd.
+		else // (m_vStack.size() > 0)
 		{
 			RepeatCmd oRepeatCmd = (RepeatCmd) m_vStack.lastElement();
 
-			// Add to the loop.
+			// Add to the repeat cmd.
 			oRepeatCmd.add(oCmd);
 
-			// Not end of loop?
-			if (!(oCmd instanceof EndRepeatCmd))
-				return;
+			// Start a new cmd block.
+			if (oCmd instanceof RepeatCmd)
+				m_vStack.push(oCmd);
 
-			EndRepeatCmd oEndCmd = (EndRepeatCmd) oCmd;
-
-			// Pop the finished loop off the stack.
-			m_vStack.pop();
-			oEndCmd.setRepeatCmd(oRepeatCmd);
-			oCmd = oRepeatCmd;
-
-			// Not bottom of stack?
-			if (m_vStack.size() > 0)
-			{
-				RepeatCmd oRepeatCmd2 = (RepeatCmd) m_vStack.lastElement();
-
-				// Add to the loop.
-				oRepeatCmd2.add(oCmd);
-
-				return;
-			}
+			// Pop repeat cmd off the stack.
+			if (oCmd instanceof EndRepeatCmd)
+				m_vStack.pop();
 		}
-
-		if (m_oFirstCmd == null)
-			m_oFirstCmd = oCmd;
-		else
-			m_oLastCmd.setNext(oCmd);
-
-		m_oLastCmd = oCmd;
 	}
 
 	/********************************************************************************
 	** Start the thread to execute the program.
 	*/
 
-	public void execute(Turtle oTurtle, int nInterval)
+	public void execute(Component oFrame, Turtle oTurtle, int nInterval)
 	{
-		oTurtle.reset();
+		oTurtle.reset(oTurtle.isVisible());
 
 		m_oThread   = new Thread(this, "Execute");
 		m_oTurtle   = oTurtle;
 		m_nInterval = nInterval;
+		m_oFrame    = oFrame;
 
 		m_oThread.start();
+	}
+
+	/********************************************************************************
+	** Execute the program using a different context.
+	*/
+
+	public void execute(Component oFrame, ExecContext oContext)
+	{
+		notifyListeners(STARTED);
+
+		try
+		{
+			Turtle oTurtle = oContext.getTurtle();
+
+			oTurtle.reset(oTurtle.isVisible());
+
+			m_vCmds.execute(oContext);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+
+			MsgBox.alert(oFrame, "JLogo", e.toString());
+		}
+
+		notifyListeners(STOPPED);
 	}
 
 	/********************************************************************************
@@ -96,26 +106,28 @@ public class Program
 	}
 
 	/********************************************************************************
-	** Execute the program, pausing between each command.
+	** Execute the program on a separate thread.
 	*/
 
 	public void run()
 	{
-		notifyListeners(START);
+		notifyListeners(STARTED);
 
-		for (Cmd oCmd = m_oFirstCmd; oCmd != null; oCmd = oCmd.getNext(true))
+		try
 		{
-			try
-			{
-				m_oThread.sleep(m_nInterval);
-			}
-			catch(InterruptedException e)
-			{ }
+			ExecContext oContext = new ExecContext(m_oTurtle, m_oThread, m_nInterval);
 
-			oCmd.execute(m_oTurtle);
+			oContext.pause();
+			m_vCmds.execute(oContext);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+
+			MsgBox.alert(m_oFrame, "JLogo", e.toString());
 		}
 
-		notifyListeners(STOP);
+		notifyListeners(STOPPED);
 
 		m_oThread = null;
 		m_oTurtle = null;
@@ -125,29 +137,13 @@ public class Program
 	** Convert the program to source code.
 	*/
 
-	public String getSource()
+	public SourceLines getSource()
 	{
-		StringBuffer strSrc = new StringBuffer(256);
-		int          nIndent = 0;
+		SourceLines oLines = new SourceLines();
 
-		for (Cmd oCmd = m_oFirstCmd; oCmd != null; oCmd = oCmd.getNext(false))
-		{
-			if (oCmd instanceof EndRepeatCmd)
-				nIndent -=4 ;
+		m_vCmds.getSource(oLines);
 
-			if (nIndent > 0)
-			{
-				for (int i = 0; i < nIndent; i++)
-					strSrc.append(' ');
-			}
-
-			strSrc.append(oCmd.getSource());
-
-			if (oCmd instanceof RepeatCmd)
-				nIndent +=4 ;
-		}
-
-		return strSrc.toString();
+		return oLines;
 	}
 
 	/********************************************************************************
@@ -168,7 +164,7 @@ public class Program
 	** Notify event listeners of the event.
 	*/
 
-	private void notifyListeners(String strEvent)
+	public void notifyListeners(String strEvent)
 	{
 		if (m_oListeners != null)
 			m_oListeners.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, strEvent));
@@ -179,22 +175,31 @@ public class Program
 	*/
 
 	// Event types.
-	public static final String START = "START";
-	public static final String STOP  = "STOP";
+	public static final String EDITED  = "EDITED";
+	public static final String STARTED = "STARTED";
+	public static final String STOPPED = "STOPPED";
+	public static final String CLEARED = "CLEARED";
+
+	// Program speeds.
+	public static final int SPEED_FACTOR = 150;
+
+	public static final int FAST    = (0 * SPEED_FACTOR);
+	public static final int MEDIUM  = (1 * SPEED_FACTOR);
+	public static final int SLOW    = (2 * SPEED_FACTOR);
 
 	/********************************************************************************
 	** Members.
 	*/
 
 	// The command list.
-	private Cmd		m_oFirstCmd;
-	private Cmd		m_oLastCmd;
-	private Stack	m_vStack = new Stack();
+	private CmdBlock	m_vCmds  = new CmdBlock();
+	private Stack		m_vStack = new Stack();
 
 	// The execute thread.
-	private Thread	m_oThread;
-	private Turtle	m_oTurtle;
-	private int		m_nInterval;
+	private Thread		m_oThread;
+	private Turtle		m_oTurtle;
+	private int			m_nInterval;
+	private Component	m_oFrame;
 
 	// Event listeners.
 	private ActionListener m_oListeners;
